15c22d656fe82b45e13f6a354dd0746e72b30b39
Sindre Sorhus <sindresorhus@gmail.com>
2015-12-31 21:13:41 +0100

    update tests for latest AVA version

--- test.js
+++ test.js
@@ -1,10 +1,8 @@
-'use strict';
-var test = require('ava');
-var revHash = require('./');
+import test from 'ava';
+import fn from './';
 
-var fixture = new Buffer('Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium.');
+const fixture = new Buffer('Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium.');
 
-test(function (t) {
-	t.assert(revHash(fixture) === 'bb9d8fe615');
-	t.end();
+test(t => {
+	t.is(fn(fixture), 'bb9d8fe615');
 });
