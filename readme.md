b767af3de7e93010ccd94e4722bf338078d1538a
Roy-Orbison <Roy-Orbison@users.noreply.github.com>
2017-06-23 22:07:23 +0930

    Accept string input (#3)

--- readme.md
+++ readme.md
@@ -2,7 +2,9 @@
 
 > Create a hash for file revving
 
-It will create a `md5` hash from the input buffer and slice it to 10 characters, which is unique enough for this purpose. If you think you need a different hash algorithm or a longer hash, [you're wrong](http://blog.risingstack.com/automatic-cache-busting-for-your-css/).
+It will create an `md5` hash from an input buffer or string, and truncate it to 10 characters, which is unique enough for this purpose.
+
+If you think you need a different hash algorithm or a longer hash, [you're wrong](http://blog.risingstack.com/automatic-cache-busting-for-your-css/).
 
 
 ## Install
@@ -21,6 +23,9 @@ var buffer = fs.readFileSync('unicorn.png');
 
 revHash(buffer);
 //=> 'bb9d8fe615'
+
+revHash(revableContentString);
+//=> '9e107d9d37'
 ```
 
 
--- test.js
+++ test.js
@@ -1,8 +1,10 @@
 import test from 'ava';
 import fn from './';
 
-const fixture = new Buffer('Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium.');
+const fixture = 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium.';
+const fixtureBuf = new Buffer(fixture);
 
 test(t => {
 	t.is(fn(fixture), 'bb9d8fe615');
+	t.is(fn(fixtureBuf), 'bb9d8fe615');
 });
