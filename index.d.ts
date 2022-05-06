3d554e9d263cc5018dffd1f173992040869b7dbe
Lete <me@imlete.cn>
2022-05-06 21:32:55 +0800

    Accept custom size

--- index.d.ts
+++ index.d.ts
@@ -4,6 +4,7 @@ import {Buffer} from 'node:buffer';
 Create a hash for file revving.
 
 @param data - The data to create a hash from.
+@param size - The size of the generated hash, default 10, maximum 32
 
 @example
 ```
@@ -15,6 +16,9 @@ revisionHash(fs.readFileSync('unicorn.png'));
 
 revisionHash('Lorem ipsum dolor sit amet');
 //=> 'fea80f2db0'
+
+revisionHash('Lorem ipsum dolor sit amet', 16);
+//=> 'fea80f2db003d4eb'
 ```
 */
-export default function revisionHash(data: Buffer | string): string;
+export default function revisionHash(data: Buffer | string, size?: number): string;
