fe9b4dcbce043e553906b0148a1edd5572e6a976
Lete <me@imlete.cn>
2022-05-06 21:31:44 +0800

    Accept custom size

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 import crypto from 'node:crypto';
 import {Buffer} from 'node:buffer';
 
-export default function revisionHash(data) {
+export default function revisionHash(data, size) {
 	if (typeof data !== 'string' && !Buffer.isBuffer(data)) {
 		throw new TypeError('Expected a Buffer or string');
 	}
 
-	return crypto.createHash('md5').update(data).digest('hex').slice(0, 10);
+	size = Number.isInteger(size) ? size : 10;
+	const md5 = crypto.createHash('md5').update(data).digest('hex');
+	return size > md5.length ? md5 : md5.slice(0, size);
 }
